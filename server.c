b13cfda2ebc80a4132394036b89238f6bb0ed0b5
Colorblind22 <36722245+Colorblind22@users.noreply.github.com>
2023-01-04 09:06:25 -0600

    basics

---
+++ server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+int main(int argc, char* argv[])
+{
+    //socket
+    //bind
+    //listen
+
+    //accept
+    //send
+    //close
+    return 0;
+}
---
+++ test.sh
@@ -0,0 +1,7 @@
+(
+gcc sockets.c -o sockets.exe
+./sockets.exe
+./sockets.exe client
+./sockets.exe server
+./sockets.exe neither
+) > testresults.txt
