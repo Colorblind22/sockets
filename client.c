92d928f9564c38132a4d0b74c5626d5acbe042db
Colorblind22 <36722245+Colorblind22@users.noreply.github.com>
2023-01-09 09:01:43 -0600

    connection working yippee

--- client.c
+++ client.c
@@ -1,39 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #define BUFFER_LENGTH 256
-#define SERVER_PORT 56542
-#define SERVER_NAME "name_of_server"
+#define SERVER_PORT 8080
+#define SERVER_NAME "127.0.0.1"
 
+
+void die(int sd)
+{
+    close(sd);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char* argv[])
 {
     struct sockaddr_in serveraddr;
+    struct addrinfo hints, *res;
     char server[BUFFER_LENGTH];
     
     // socket
     int socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    printf("sockfs: %d\n", socket_descriptor);
 
     if(argc > 1)
         strcpy(server, argv[1]);
     else
         strcpy(server, SERVER_NAME);
 
+    printf("server name: %s\n", server);
+
+    // set up server info
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(SERVER_PORT);
-    if(inet_pton(AF_INET, server, &serveraddr.sin_addr.s_addr) != -1)
-    {
-        
-    }
+    //if(
+    inet_pton(AF_INET, server, &serveraddr.sin_addr);
+    // != -1)
+    /*{
+        printf("retrieve ip of server...\n");
+        memset(&hints, 0, sizeof(hints));
+        hints.ai_family = AF_INET;
+        hints.ai_flags = AI_V4MAPPED;
+        getaddrinfo(server, NULL, &hints, &res);
+
+        memcpy
+        (
+            &serveraddr.sin_addr,
+            (&((struct sockaddr_in *)(res->ai_addr))->sin_addr),
+            sizeof(serveraddr.sin_addr)
+        );
+
+        freeaddrinfo(res);
+        printf("server ip retrieved\n");
+    }*/
 
     // bind
     // connect
+    printf("connecting...\n");
+    if(connect(socket_descriptor, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+    {
+        perror("connect() failed");
+        die(socket_descriptor);
+    }
+    printf("connected\n");
 
     // send
     // recv
--- server.c
+++ server.c
@@ -3,29 +3,33 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/poll.h>
+#include <unistd.h>
 //#include <sys/types.h>
 
 #define BUFFER_LENGTH 256
-#define PORT 56542
+#define PORT 8080
 
 int main(int argc, char* argv[])
 {
     int on=1;
     struct sockaddr_in serveraddr;
     
-    //socket
-        //referred to in guide as sockfd for socket file descriptor
-    int socket_descriptor = socket(/*domain*/AF_INET, /*type*/SOCK_STREAM, /*protocol*/0);
-    printf("sockfs: %d\n", socket_descriptor);
-    /*
+    /*socket
+        referred to in guide as sockfd for socket file descriptor
+    
         AF_INET specifies ipv4
         SOCK_STREAM specifies TCP
         0 is what it told me to use idk
             - $ man protocols
             - /etc/protocols
     */
+    int socket_descriptor = socket(/*domain*/AF_INET, /*type*/SOCK_STREAM, /*protocol*/0);
+    printf("sockfs: %d\n", socket_descriptor);
     // setsockopt to help in reuse of address and port
-    printf("setsockopt: %d\n", setsockopt(socket_descriptor, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)));
+    printf("setsockopt: %d\n", setsockopt(socket_descriptor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &on, sizeof(on)));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = INADDR_ANY;
+    serveraddr.sin_port = htons(PORT);
 
     //bind
     printf("bind: %d\n", bind(socket_descriptor, (struct sockaddr *)&serveraddr, sizeof(serveraddr)));
@@ -39,8 +43,8 @@ int main(int argc, char* argv[])
     int comm_socket_descriptor = accept(socket_descriptor, NULL, NULL);
     printf("accept(): %d\n", comm_socket_descriptor);
     struct pollfd fd;
-    nfds_t n_fd = 1;
     // number of file descriptors
+    nfds_t n_fd = 1;
     int recieve = poll(&fd, n_fd, 30000);
     printf("poll(): %d\n", recieve);
 
